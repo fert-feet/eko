5d03d4828bef1d25b40ccd5b785dc0c6762c4f86
fert-feet <3214588281@qq.com>
2025-10-30 21:17:24 +0800

    feat(widget): add widget header and update footer navigation

--- apps/widget/modules/widget/ui/components/widget-footer.tsx
+++ apps/widget/modules/widget/ui/components/widget-footer.tsx
@@ -16,8 +16,15 @@ const WigetFooter = () => {
                 <HomeIcon
                     className={cn("size-5", screen === "selection" && "text-primary")}
                 />
+            </Button>
+            <Button
+                className="h-14 flex-1 rounded-none"
+                onClick={() => { }}
+                size={"icon"}
+                variant={"ghost"}
+            >
                 <InboxIcon
-                    className={cn("size-5", screen === "inbox" && "text-primary")}
+                    className={cn("size-5", screen === "selection" && "text-primary")}
                 />
             </Button>
         </footer>
--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import WigetFooter from "../components/widget-footer";
+import WigetHeader from "../components/widget-header";
 
 interface Props {
     organizationId: string;
@@ -9,10 +10,23 @@ interface Props {
 const WidgetView = ({ organizationId }: Props) => {
     return (
         <>
-            <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
-                Wiget View: {organizationId}
+            <main className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted">
+                <WigetHeader>
+                    
+                    <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
+                        <p className="text-3xl">
+                            Hi there! 👋
+                        </p>
+                        <p className="text-lg">
+                            How can we help you today?
+                        </p>
+                    </div>
+                </WigetHeader>
+                <div className="flex flex-1">
+                    Wiget View: {organizationId}
+                </div>
+                <WigetFooter />
             </main>
-            <WigetFooter />
         </>
     );
 };
