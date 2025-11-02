c7c1a7fa7d9a79e2b2b462fbeec4f915177bd08a
fert-feet <3214588281@qq.com>
2025-11-02 16:47:59 +0800

    feat(widget): add error screen

--- apps/widget/modules/widget/atoms/widget-atoms.tsx
+++ apps/widget/modules/widget/atoms/widget-atoms.tsx
@@ -1,4 +1,7 @@
 import { atom } from "jotai";
 import { WidgetScreen } from "@/modules/widget/types";
 
+// basic state atoms
 export const screenAtom = atom<WidgetScreen>("auth")
+
+export const errorMessageAtom = atom<string | null>(null)
